f66f5207669bcb5aec9c800e9e5f80d2ef8f1e77
simrankailey <simrankailey536@gmail.com>
2025-08-31 19:52:11 +0530

    Update db.sql

--- asset/db/db.sql
+++ asset/db/db.sql
@@ -1,27 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
---
--- Host: localhost    Database: counseling_service
--- ------------------------------------------------------
--- Server version	8.0.33
+-- Cleaned MySQL Dump for counseling_service
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET NAMES utf8mb4;
+SET time_zone = '+00:00';
 
 --
 -- Table structure for table `contacts`
 --
 
 DROP TABLE IF EXISTS `contacts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contacts` (
   `c_id` int NOT NULL AUTO_INCREMENT,
   `c_name` varchar(100) NOT NULL,
@@ -30,25 +16,23 @@ CREATE TABLE `contacts` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`c_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `contacts`
 --
 
-LOCK TABLES `contacts` WRITE;
-/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
-INSERT INTO `contacts` VALUES (1,'simi','simrankailet23@gmail.com','hello','2025-08-06 16:26:51'),(2,'edfnsj','aaf@gmail.com','afaetaet','2025-08-06 16:30:02'),(3,'siya','siya@gmail.com','gsdga','2025-08-06 16:37:45'),(4,'edfnsj','siya@gmail.com','wfasfas','2025-08-06 16:40:15'),(5,'sita','aaf@gmail.com','fsgsegsg','2025-08-06 16:42:03');
-/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `contacts` VALUES 
+(1,'simi','simrankailet23@gmail.com','hello','2025-08-06 16:26:51'),
+(2,'edfnsj','aaf@gmail.com','afaetaet','2025-08-06 16:30:02'),
+(3,'siya','siya@gmail.com','gsdga','2025-08-06 16:37:45'),
+(4,'edfnsj','siya@gmail.com','wfasfas','2025-08-06 16:40:15'),
+(5,'sita','aaf@gmail.com','fsgsegsg','2025-08-06 16:42:03');
 
 --
 -- Table structure for table `counselling_requests`
 --
 
 DROP TABLE IF EXISTS `counselling_requests`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `counselling_requests` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -63,25 +47,19 @@ CREATE TABLE `counselling_requests` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `counselling_requests`
 --
 
-LOCK TABLES `counselling_requests` WRITE;
-/*!40000 ALTER TABLE `counselling_requests` DISABLE KEYS */;
-INSERT INTO `counselling_requests` VALUES (2,'poonam','poonam@poonam.com','7814417338','Commerce','Science','Engineer','Abroad','testing it','no','2025-08-31 13:21:43');
-/*!40000 ALTER TABLE `counselling_requests` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `counselling_requests` VALUES 
+(2,'poonam','poonam@poonam.com','7814417338','Commerce','Science','Engineer','Abroad','testing it','no','2025-08-31 13:21:43');
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
@@ -92,25 +70,16 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'simran','simrankailey536@gmail.com','12345','2025-08-06 14:04:50',0),(3,'neelam','neelam@gmail.com','12345','2025-08-06 17:22:37',0),(4,'kashish','kashish@gmail.com','12345','2025-08-08 08:44:16',0),(5,'poonam','poonam@gmail.com','poonam','2025-08-15 17:33:24',0),(6,'admin user','admin@admin.com','password','2025-08-31 11:25:41',1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` VALUES 
+(2,'simran','simrankailey536@gmail.com','12345','2025-08-06 14:04:50',0),
+(3,'neelam','neelam@gmail.com','12345','2025-08-06 17:22:37',0),
+(4,'kashish','kashish@gmail.com','12345','2025-08-08 08:44:16',0),
+(5,'poonam','poonam@gmail.com','poonam','2025-08-15 17:33:24',0),
+(6,'admin user','admin@admin.com','password','2025-08-31 11:25:41',1);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-08-31 19:08:51
+-- End of dump
